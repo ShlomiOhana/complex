88bc4d806e0f60165df87958da98a4d29dfe01ff
Shlomi_O <shlomio@galcon.co.il>
2024-01-23 12:51:49 +0200

    first commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const keys = require('./keys');
+
+// Express App Setup
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Postgres Client Setup
+const { Pool } = require('pg');
+const pgClient = new Pool({
+    user: keys.pgUser,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    password: keys.pgPassword,
+    port: keys.pgPort,
+    ssl:
+      process.env.NODE_ENV !== 'production'
+        ? false
+        : { rejectUnauthorized: false },
+  });
+  pgClient.on('error', ()=> console.log("Lost PG connection"));
+
+  pgClient.query('CREATE TABLE IF NOT EXIST values (number INT)')
+  .catch((err) => console.log(err));
