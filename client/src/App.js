de98e8543c4dd33d54c05783801393a6ba1ee72b
ShlomiOhana <shlomioh91@gmail.com>
2024-01-30 14:59:27 +0200

    added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './otherPage';
+import Fib from './fib';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link to="/">Home</Link>
+        <Link to="/otherPage">Other Page</Link>
       </header>
+      <div>
+        <Route exact path="/" component={Fib}/>
+        <Route exact path="/otherPage" component={OtherPage}/>
+      </div>
     </div>
+    </Router>
   );
 }
 
